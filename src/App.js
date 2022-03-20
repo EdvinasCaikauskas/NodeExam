0b3fe915512462b7c0efa8f3243a054905b44b16
EdvinasCaikauskas <edvinas.cai@gmail.com>
2022-03-20 22:48:59 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {useState} from "react";
+import mainContext from "./context/mainContext";
+import Toolbar from "./comps/Toolbar";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import UploadPage from "./pages/UploadPage";
+import SearchPage from "./pages/SearchPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState(null)
+
+    return (
+        <div className="main">
+            <mainContext.Provider value={{user, setUser}}>
+                <BrowserRouter>
+                    <Toolbar/>
+                    <Routes>
+                        <Route path='/' element={<HomePage/>}/>
+                        <Route path='/auth' element={<AuthPage/>}/>
+                        <Route path='/upload' element={<UploadPage/>}/>
+                        <Route path='/search' element={<SearchPage/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </mainContext.Provider>
+        </div>
+    );
 }
 
 export default App;
